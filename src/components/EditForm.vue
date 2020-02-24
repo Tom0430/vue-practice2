c8c3423b30a9ae3dc67a93496a29a6d20eb566f0
tomoya takahashi <tiger@tomoyanoMacBook-Pro.local>
2020-02-24 15:11:12 +0900

    chapter8途中 stateの変更

--- src/components/EditForm.vue
+++ src/components/EditForm.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="edit-form">
         <input type="text" :value="message" @input="doUpdate">
+        <input v-model="message2">
     </div>
 </template>
 <!-- :はv-bindの、＠はv-onの省略記法ここのvalueをdoUpdateのevent.targetでみている -->
@@ -9,7 +10,12 @@
 export default {
     name:'EditForm',
     computed:{
-        message() { return this.$store.getters.message }
+        message() { return this.$store.getters.message },
+        // 1つの算出プロパティに入出力をまとめるとスッキリする
+        message2: {
+            get()      { return this.$store.getters.message},
+            set(value) { this.$store.dispatch('doUpdate', value )}
+        },
     },
     methods:{
         doUpdate(event){
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,39 @@
 <template>
-  <p>{{ text }}</p>
+  <div class="message-field">
+    <p>{{ text }}</p>
+    <p>{{ message3 }}</p>
+    <input v-model="message3">
+    <p>{{ count }}</p>
+    <button v-on:click="increment"></button>
+  </div>
 </template>
 
 <script>
 import store from '@/store.js'
 export default {
   name: 'Example',
+
   data(){
     return{
-      text: 'example'
+      text: 'こんにちは'
     }
   },
+  computed:{
+    message3:{
+      get() { return this.$store.state.moduleA.message3 },
+      set(value) { return this.$store.dispatch('message3Update',value) }
+      //setにはmoduleAとか書かなくて勝手に見てくれる
+      //もし同じミューテーションタイプになってたら（全部messageにしてたら）全て一緒に書き換わる
+    },
+    count:{
+      get() {return this.$store.state.moduleB.count}
+      //getterだけでも使える
+    },
+  },
+  methods:{
+    increment(){
+      this.$store.commit('countUpdate')
+    }
+  }
 }
 </script>
